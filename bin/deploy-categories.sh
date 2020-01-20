f8e549b8689d3bd66059b4f5472aa5397c3c34af
Avi Goldman <avrahamymgoldman@gmail.com>
2020-01-19 21:26:32 -0500

    added support for nested categories and the 3 types of categories: list, custom, and rollup

--- bin/deploy-categories.sh
+++ bin/deploy-categories.sh
@@ -20,27 +20,85 @@ fi
 # push em in to wordpress
 for directory in "${CHANGED_DIRECTORIES[@]}"; do
   slug=$(slugify "$(basename "$directory")")
-  print_title "$slug"
+  parent="$(basename "$(dirname "$directory")")"
+  wp_parent_id=""
 
+  # remove the parent value if it is a top level directory in the content directory
+  if [[ "$DIRECTORY" != "$parent" ]] ; then
+    wp_parent_index=$(index_of "${WP_CATEGORY_SLUGS[@]}" "$parent")
+
+    # if we don't have the parent category it is likely that it was created in the same pr
+    # so we need to refresh our cache of category info
+    if [ "-1" == "$wp_parent_index" ]; then
+      refresh_categories
+      wp_parent_index=$(index_of "${WP_CATEGORY_SLUGS[@]}" "$parent")
+    fi
+
+    wp_parent_id=${WP_CATEGORY_IDS[$wp_parent_index]}
+  fi
+
+  print_title "$slug"
+  
   wp_cat_index=$(index_of "${WP_CATEGORY_SLUGS[@]}" "$slug")
 
   # get the category markdown
   md_cat=$(node bin/markdown.js $directory/index.md)
+  md_cat_images=("$(echo $md_cat | jq --compact-output --raw-output '.images')")
   md_cat_name="$(echo $md_cat | jq '.meta.name' --raw-output)"
   md_cat_description="$(echo $md_cat | jq '.meta.description' --raw-output)"
   [ "$md_cat_description" == "null" ] && md_cat_description="" || md_cat_description=$md_cat_description
+  md_cat_type="$(echo $md_cat | jq '.meta.type' --raw-output)"
+
+  if [ "$md_cat_type" == "null" ]; then
+    md_cat_type=''
+  fi
 
   # create
   if [ "-1" == "$wp_cat_index" ] && [ -f "$directory/index.md" ]; then
       echo " - Creating category"
-      echo " - $(do_wp term create "$WP_CUSTOM_TAX" "$md_cat_name" --slug="$slug" --description="$md_cat_description")"
+
+      wp_cat_id=$(do_wp term create "$WP_CUSTOM_TAX" "$md_cat_name" --slug="$slug" --description="$md_cat_description" --parent="$wp_parent_id" --porcelain)
+      
+      if [[ $wp_cat_id =~ $NUMBER_PATTERN ]]; then
+        echo " - Success: Created category $wp_cat_id."
+
+        # add type
+        echo " - $(do_wp term meta add "$wp_cat_id" "type" "$md_cat_type")"
+
+        if [ "$md_cat_type" == "custom" ]; then
+          # add contributors
+          contributors=$(get_contributors "$directory/index.md")
+          echo " - $(do_wp term meta add "$wp_cat_id" "contributors" "$contributors")"
+
+          # add the content
+          md_cat_content=$(generate_html "$directory/index.md")
+
+          echo " - $(do_wp term meta add "$wp_cat_id" "content" "$md_cat_content")"
+        fi
+      else
+        echo " - $wp_cat_id"
+      fi
   fi
 
   # update
   if [ "-1" != "$wp_cat_index" ] && [ -f "$directory/index.md" ]; then
     wp_cat_id=${WP_CATEGORY_IDS[$wp_cat_index]}
     echo " - Updating category"
-    echo " - $(do_wp term update "$WP_CUSTOM_TAX" "$wp_cat_id" --name="$md_cat_name" --description="$md_cat_description")"
+    echo " - $(do_wp term update "$WP_CUSTOM_TAX" "$wp_cat_id" --name="$md_cat_name" --description="$md_cat_description" --parent="$wp_parent_id")"
+    
+    # add type
+    echo " - $(do_wp term meta update "$wp_cat_id" "type" "$md_cat_type")"
+
+    if [ "$md_cat_type" == "custom" ]; then
+      # add contributors
+      contributors=$(get_contributors "$directory/index.md")
+      echo " - $(do_wp term meta update "$wp_cat_id" "contributors" "$contributors")"
+
+      # add the content
+      md_cat_content=$(generate_html "$directory/index.md")
+
+      echo " - $(do_wp term meta update "$wp_cat_id" "content" "$md_cat_content")"
+    fi
   fi
 
   # delete
