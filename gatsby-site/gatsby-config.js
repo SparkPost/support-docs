c29fd9e21a40ff87079d85218f74e5716ffc9925
Evan Young <82669192+EvanYoung-SP@users.noreply.github.com>
2021-05-27 10:32:29 -0700

    WE-152: create gatsby instance (#503)
    * Installs Gatsby into the support-docs repo. (WIP)
    
    * Creates a new gatsby instance and adds google tracking to the gatsby-config file.
    
    * Removes submodule
    
    * Updates gatsby to latest version (3.6.0)

---
+++ gatsby-site/gatsby-config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  siteMetadata: {
+    title: "Support Documents",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-53870770-1",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.md`, `.mdx`],
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
