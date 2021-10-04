47722c4622fd2ba0810490ba4b4b570266827f1a
Max Harris <maxreuven@gmail.com>
2021-10-04 11:03:12 -0700

    initializing app and content

--- gatsby-site/gatsby-node.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// Edit the ts files in the ts-configs, not the js files in the root since those will be overwritten by the ts files
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createPages = void 0;
-var create_pages_1 = require("./create-pages");
-Object.defineProperty(exports, "createPages", { enumerable: true, get: function () { return create_pages_1.createPages; } });
