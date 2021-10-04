f3e8c4a09ee39b4d11e9729340e6a5e61b4516ef
Max Harris <maxreuven@gmail.com>
2021-10-04 13:58:27 -0700

    initializing app

--- bin/silent.php
+++
@@ -1,1 +0,0 @@
-<?php error_reporting(0); @ini_set('display_errors', 0);
--- gatsby-site/ts-configs/gatsby-node.ts
+++
@@ -1,3 +0,0 @@
-// Edit the ts files in the ts-configs, not the js files in the root since those will be overwritten by the ts files
-
-export { createPages } from "./create-pages"
