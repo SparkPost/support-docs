417262b6309ec29b5fc9fe95613ef73520372532
Max Harris <maxreuven@gmail.com>
2021-10-05 10:59:33 -0700

    installing Mathcbox and creating test page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@sparkpost/matchbox'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ThemeProvider><Component {...pageProps} /></ThemeProvider>;
 }
 export default MyApp
