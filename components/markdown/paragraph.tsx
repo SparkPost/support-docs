707aceecb96d4b71708ed95bb433488826645d25
Jon Ambas <jon@jonambas.com>
2021-10-07 09:44:50 -0400

    WE-274 Remove a console log

--- components/markdown/paragraph.tsx
+++ components/markdown/paragraph.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Box, BoxProps } from '@sparkpost/matchbox';
 
 function Paragraph(props: BoxProps): JSX.Element {
-  console.log(props);
   return <Box mb="400">{props.children}</Box>;
 }
 
