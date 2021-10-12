825e85f19005675739a5a290fb7f22933efbbba1
Jon Ambas <jon@jonambas.com>
2021-10-12 13:39:55 -0400

    WE-295 Add Momentum side navigation architecture

--- lib/api.ts
+++ lib/api.ts
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import glob from 'glob';
 import matter, { GrayMatterFile } from 'gray-matter';
+import yaml from 'js-yaml';
 
 export const MOMENTUM_PATH: string = path.join(process.cwd(), 'content/momentum/');
 export const SUPPORT_PATH: string = path.join(process.cwd(), 'content/support/');
@@ -26,18 +27,32 @@ export const getAllMomentumPostPaths = (): string[] => {
 /**
  * Retrieves a single Momentum post from a slug.
  */
-export const getSingleMomentumPost = (slug: string[] | string): GrayMatterFile<Buffer> | void => {
+export const getSingleMomentumPost = (slug: string[] | string): GrayMatterFile<string> | void => {
   const postPath = typeof slug === 'string' ? slug : slug.join('/');
   const filePath = path.join(MOMENTUM_PATH, `${postPath}.md`);
   const indexPath = path.join(MOMENTUM_PATH, `${postPath}/index.md`);
 
   // If file exists, it is not an index page.
   if (fs.existsSync(filePath)) {
-    return matter(fs.readFileSync(filePath));
+    return matter(fs.readFileSync(filePath, 'utf8'));
   }
 
   if (fs.existsSync(indexPath)) {
-    return matter(fs.readFileSync(indexPath));
+    return matter(fs.readFileSync(indexPath, 'utf8'));
+  }
+
+  return;
+};
+
+/**
+ * Retrieves momentum navigation structure as JSON.
+ */
+export const getMomentumNavigation = (): any | void => {
+  const filePath = path.join(MOMENTUM_PATH, `navigation.yml`);
+
+  if (fs.existsSync(filePath)) {
+    const data = fs.readFileSync(filePath, 'utf8');
+    return yaml.load(data);
   }
 
   return;
--- pages/momentum/index.tsx
+++ pages/momentum/index.tsx
@@ -1,5 +1,29 @@
-function IndexPage(): JSX.Element {
-  return <>Momentum Index Page</>;
-}
+import { GetStaticProps } from 'next';
+import { getMomentumNavigation } from 'lib/api';
+import MomentumNavigation, { ItemProps } from 'components/site/momentumNavigation';
+import { Box } from '@sparkpost/matchbox';
+
+type IndexPageProps = {
+  data: {
+    navigation?: ItemProps[];
+  };
+};
+
+const IndexPage = (props: IndexPageProps): JSX.Element => {
+  const { data } = props;
+  return (
+    <Box display="flex">
+      <Box flex="0">
+        <MomentumNavigation data={data.navigation} />
+      </Box>
+      <Box flex="1"></Box>
+    </Box>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const navigation = getMomentumNavigation() || null;
+  return { props: { data: { navigation } } };
+};
 
 export default IndexPage;
