fcf7b60ec3f6bd8ffbbf9f27971d7adc51ee6582
Evan Young <evan.young@sparkpost.com>
2021-10-12 18:29:20 -0700

    Adds production ready version of google analytics tracking. Need to verify it's working.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,47 @@
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
 import { ThemeProvider } from '@sparkpost/matchbox';
+import * as gtag from '../utils/gtag';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      {process.env.NODE_ENV === 'production' && (
+        <>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </>
+      )}
       <Component {...pageProps} />
     </ThemeProvider>
   );
