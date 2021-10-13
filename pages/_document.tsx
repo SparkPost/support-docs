b0e02aa58f10db18f3229a51d8b847c47ed8fcf7
Evan Young <evan.young@sparkpost.com>
2021-10-13 13:37:59 -0700

    Uses better grammar ^^;

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -64,7 +64,7 @@ class AppDocument extends Document {
               {/* Google Tag Manager (noscript) */}
               <noscript>
                 <iframe
-                  title="Google Tag manager (noscript)"
+                  title="Google Tag Manager (noscript)"
                   src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
                   height="0"
                   width="0"
