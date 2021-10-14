e9e9c8b201e08ed7cbb3360674a0816451cd4745
Evan Young <evan.young@sparkpost.com>
2021-10-13 19:06:09 -0700

    Fixes a bug that was being caused by a CORS issue when sentry was introduced to the google analytics implementation.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,34 +3,34 @@ import { useEffect } from 'react';
 import Script from 'next/script';
 import { useRouter } from 'next/router';
 import { ThemeProvider } from '@sparkpost/matchbox';
-import * as gtag from '../utils/gtag';
+import * as gtag from '../utils/gtm';
 import * as Sentry from '@sentry/react';
 import { Integrations } from '@sentry/tracing';
 
 /**
  * Using the React sentry integration instead of
  * their next.js integration.
- *
  */
-Sentry.init({
-  dsn: 'https://3b9d989425bd4cf0892613a46c2f9804@o104929.ingest.sentry.io/6004403',
-  integrations: [new Integrations.BrowserTracing()],
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: 'https://3b9d989425bd4cf0892613a46c2f9804@o104929.ingest.sentry.io/6004403',
+    integrations: [new Integrations.BrowserTracing()],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
 
-  // Release is used to match events with uploaded sourcemaps
-  release: process.env.RELEASE,
-});
+    // Release is used to match events with uploaded sourcemaps
+    release: process.env.RELEASE,
+  });
+}
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   const router = useRouter();
   useEffect(() => {
     const handleRouteChange = (url: URL) => {
-      gtag.gaPageview(url, process.env.NEXT_PUBLIC_GA_ID || 'nothing_to_see_here');
-      gtag.gtmPageview(url);
+      gtag.pageview(url);
     };
     router.events.on('routeChangeComplete', handleRouteChange);
     return () => {
@@ -42,27 +42,6 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ThemeProvider>
       {process.env.NODE_ENV === 'production' && (
         <>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <Script
-            strategy="afterInteractive"
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-          />
-          <Script
-            id="gtag-init"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
-          {/* End Global Site Tag (gtag.js) - Google Analytics */}
-          {/* Google Tag Manager */}
           <Script
             strategy="afterInteractive"
             dangerouslySetInnerHTML={{
@@ -70,7 +49,7 @@ function MyApp({ Component, pageProps }: AppProps) {
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer', '${process.env.NEXT_PUBLIC_GTM_ID}');
               `,
             }}
@@ -81,5 +60,5 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Component {...pageProps} />
     </ThemeProvider>
   );
-}
+};
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -65,7 +65,7 @@ class AppDocument extends Document {
               <noscript>
                 <iframe
                   title="Google Tag Manager (noscript)"
-                  src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+                  src={`//www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
                   height="0"
                   width="0"
                   style={{ display: 'none', visibility: 'hidden' }}
