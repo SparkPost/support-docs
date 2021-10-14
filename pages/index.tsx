8fdb7ece85e13819f71077f2a899c360b2d5bf03
Logan Sparlin <logan.sparlin@gmail.com>
2021-10-14 11:41:03 -0500

    WE-277 Add meta tags (#559)
    * WE-277 add SEO meta tags
    
    * WE-277 remove config file
    
    * WE-277 add description to momentum index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,10 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
+import SEO from 'components/site/seo';
 
 const Home: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>SparkPost Support Docs</title>
-        <meta name="description" content="SparkPost Support Docs" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <SEO />
       <main>
         There&apos;s nothing here yet
         <button
