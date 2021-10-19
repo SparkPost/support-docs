fe6af587fca6398fdf0591d81cd9174bc83cb5d5
Evan Young <82669192+EvanYoung-SP@users.noreply.github.com>
2021-10-19 13:53:22 -0700

    WE-173: integrate segment (#563)
    * Adds segment to _app.tsx, adds next/core-web-vitals to our eslintrc config for better/optimized builds, fixes a few build warnings/errors (manually importing a .css file, next.js not being in eslint configuration), updated es6 to es2021 for a more recent ecmascript env since we're starting out fresh.
    
    * Addresses the change requests made in the PR.

---
+++ components/site/segment.tsx
@@ -0,0 +1,43 @@
+import Script from 'next/script';
+import isbot from 'isbot';
+import * as snippet from '@segment/snippet';
+
+const Segment = (): JSX.Element => {
+  const isBotSSR = (): boolean => {
+    let bot = false;
+    if (typeof window !== 'undefined') {
+      bot = isbot(window.navigator.userAgent);
+    }
+
+    return bot;
+  };
+
+  const renderSnippet = () => {
+    const opts = {
+      apiKey: process.env.NEXT_PUBLIC_SEGMENT_ID,
+      // note: the page option only covers SSR tracking.
+      // Page.js is used to track other events using `window.analytics.page()`
+      page: true,
+    };
+
+    if (process.env.NODE_ENV === 'development') {
+      return snippet.max(opts);
+    }
+
+    return snippet.min(opts);
+  };
+
+  return (
+    <>
+      {process.env.NODE_ENV === 'production' && !isBotSSR() && (
+        <Script
+          id="segmentIntegration"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{ __html: renderSnippet() }}
+        />
+      )}
+    </>
+  );
+};
+
+export default Segment;
