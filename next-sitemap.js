5ea99ab31e105dbca5d36945faad91bb82a50fa9
Evan Young <evan.young@sparkpost.com>
2021-10-21 16:39:58 -0700

    Adds the next-sitemap plugin and it's configuration. Also updates the gitignore to ignore the sitemap and robots.txt builds since those will be made on build in netlify and don't need to be tracked in github.

---
+++ next-sitemap.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://support.sparkpost.com',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        disallow: ['*'],
+      },
+    ],
+  },
+};
