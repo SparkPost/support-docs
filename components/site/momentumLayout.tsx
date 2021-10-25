710f1cd59951637d93b073926f9060652f10e4f9
Logan Sparlin <logan.sparlin@gmail.com>
2021-10-25 14:47:59 -0500

    WE-163 add footer

--- components/site/momentumLayout.tsx
+++ components/site/momentumLayout.tsx
@@ -1,5 +1,6 @@
 import { Box, BoxProps } from '@sparkpost/matchbox';
 import Header from 'components/site/header';
+import Footer from 'components/site/footer';
 import MomentumNavigation, {
   MomentumNavigationItemProps,
 } from 'components/site/momentumNavigation';
@@ -24,31 +25,34 @@ const StyledSkipToContent = styled(Box)<BoxProps>`
 const MomentumLayout = (props: LayoutProps): JSX.Element => {
   const { children, navigation } = props;
   return (
-    <Box display="flex" justifyContent="center" bg="gray.100">
-      <div>
-        <StyledSkipToContent
-          as="a"
-          href="#main-content"
-          position="absolute"
-          display="inline-block"
-          p="200"
-          top="0"
-          left="0"
-          bg="blue.700"
-        >
-          Skip to main content
-        </StyledSkipToContent>
-        <Header />
-        <Box display="flex" maxWidth="1400" bg="white" border="400">
-          <Box flex="0">
-            <MomentumNavigation data={navigation} />
+    <>
+      <Box display="flex" justifyContent="center" bg="gray.100">
+        <div>
+          <StyledSkipToContent
+            as="a"
+            href="#main-content"
+            position="absolute"
+            display="inline-block"
+            p="200"
+            top="0"
+            left="0"
+            bg="blue.700"
+          >
+            Skip to main content
+          </StyledSkipToContent>
+          <Header />
+          <Box display="flex" maxWidth="1400" bg="white" border="400">
+            <Box flex="0">
+              <MomentumNavigation data={navigation} />
+            </Box>
+            <Box p="500" flex="1" id="main-content">
+              {children}
+            </Box>
           </Box>
-          <Box p="500" flex="1" id="main-content">
-            {children}
-          </Box>
-        </Box>
-      </div>
-    </Box>
+        </div>
+      </Box>
+      <Footer />
+    </>
   );
 };
 
