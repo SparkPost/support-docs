2697a6200519f206cb4b648b6a52a265e93b84a8
Jon Ambas <jon@jonambas.com>
2021-10-25 11:39:43 -0400

    WE-162 Add momentum site header

---
+++ components/site/header.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Logo from 'components/site/logo';
+import { Box, Button, TextField } from '@sparkpost/matchbox';
+import { Search } from '@sparkpost/matchbox-icons';
+import Cookies from 'js-cookie';
+
+const DASHBOARD_LINK = 'https://app.sparkpost.com';
+const LOGIN_LINK = 'https://app.sparkpost.com/auth';
+const SIGNUP_LINK =
+  'https://app.sparkpost.com/join?plan=free-0817&sfdcid=7016000000198J1&src=SP-Website';
+const AUTH_KEY = 'website_auth';
+
+const Header = () => {
+  const isLoggedIn = React.useMemo(() => {
+    return !!Cookies.get(AUTH_KEY);
+  }, []);
+
+  return (
+    <Box my="600">
+      <Box as="header" height="650" display="flex" justifyContent="space-between" mb="400">
+        <Logo />
+        {isLoggedIn ? (
+          <Button as="a" color="blue" variant="outline" href={DASHBOARD_LINK}>
+            Your Dashboard
+          </Button>
+        ) : (
+          <Button.Group>
+            <Button as="a" color="blue" variant="outline" href={LOGIN_LINK}>
+              Login
+            </Button>
+            <Button as="a" color="blue" variant="outline" href={SIGNUP_LINK}>
+              Try Now
+            </Button>
+          </Button.Group>
+        )}
+      </Box>
+      <Box maxWidth="1150">
+        <TextField
+          id="algolia-search"
+          label="Search"
+          prefix={<Search />}
+          placeholder="e.g. Getting Started"
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default Header;
