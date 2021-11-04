238d56e1301cb533ea64faead79497b3b7b86a2a
Logan Sparlin <logan.sparlin@gmail.com>
2021-11-04 15:11:58 -0500

    WE-315 Style markdown table component

---
+++ components/markdown/table.tsx
@@ -0,0 +1,18 @@
+import { Panel, Table as MBTable } from '@sparkpost/matchbox';
+
+type TableProps = {
+  children?: React.ReactNode;
+}
+
+const Table = (props: TableProps): JSX.Element => {
+  const { children } = props;
+  return (
+    <Panel>
+      <MBTable p="500">
+        {children}
+      </MBTable>
+    </Panel>
+  )
+}
+
+export default Table;
