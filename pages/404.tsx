24354d8821e05c2761defda45846b75b5394f74b
Evan Young <evan.young@sparkpost.com>
2021-11-10 16:11:18 -0800

    Finishes up the 404 page.

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,14 +1,32 @@
-import type { NextPage } from 'next';
 import Image from 'next/image';
+import type { NextPage } from 'next';
+import Link from 'next/link';
 import SEO from 'components/site/seo';
 import LayoutWrap from 'components/site/layoutWrap';
 import LayoutInnerContent from 'components/site/layoutInnerContent';
 import { Box } from '@sparkpost/matchbox';
+import styled from 'styled-components';
+import css from '@styled-system/css';
+
+const StyledLink = styled(Box)`
+  &,
+  &:visited {
+    ${css({
+      color: 'gray.900',
+    })}
+  }
+
+  &:hover {
+    ${css({
+      color: 'blue.700',
+    })}
+  }
+`;
 
 const OhhhNoes: NextPage = () => {
   const width = 1179;
   const height = 532;
-  const style = { paddingBottom: `min(350px, ${100 / (width / height)}%)` };
+  const style = { paddingBottom: `clamp(350px, ${100 / (width / height)}%, 400px)` };
   return (
     <>
       <SEO
@@ -17,17 +35,56 @@ const OhhhNoes: NextPage = () => {
       />
       <LayoutWrap bg="white">
         <LayoutInnerContent>
-          <Box display="flex" width="100%">
-            <Box as="h1" fontSize="600" lineHeight="600" fontWeight="semibold">
+          <Box display="flex" flexDirection="column" alignItems="center" width="100%">
+            <Box
+              as="h1"
+              fontSize={['800', null, '228.39px']}
+              lineHeight={['800', null, '285.49px']}
+              fontWeight="semibold"
+              mt={['0', null, '750']}
+            >
               404
             </Box>
-            <div className={`next-image-wrapper`} style={style}>
-              <Image className="next-image" src="/404.png" layout="fill" objectFit="contain" />
-            </div>
+            <Box
+              as="p"
+              fontSize={['200', null, '600']}
+              lineHeight={['200', null, '600']}
+              textAlign="center"
+              mt={['100', null, '0']}
+            >
+              Oops! Looks like we can’t find the page you’re looking for.
+            </Box>
+            <Box
+              as="p"
+              fontSize={['200', null, '600']}
+              lineHeight={['200', null, '600']}
+              textAlign="center"
+              my={['400', null, '500']}
+            >
+              Return to{' '}
+              <Link href="/docs" passHref>
+                <StyledLink as="a" href="/docs">
+                  Home
+                </StyledLink>
+              </Link>
+              .
+            </Box>
+            <Box
+              position="relative"
+              display="flex"
+              justifyContent="center"
+              width="100%"
+              mt="500"
+              mb={['700', null, '434px']}
+            >
+              <Image className="next-image" src="/404.png" width="1179" height="532" />
+            </Box>
           </Box>
+          {/* <div className={`next-image-wrapper`} style={style}> */}
+
+          {/* </div> */}
         </LayoutInnerContent>
       </LayoutWrap>
-      ;
     </>
   );
 };
