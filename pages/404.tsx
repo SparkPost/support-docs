e6eb280a48e594f61df8982caa3842f4c21f6801
Evan Young <evan.young@sparkpost.com>
2021-11-12 14:08:42 -0800

    Updates the overall height of the 404 content to fit inside the window height above the fold, makes the font-size dynamic and uses rems instead of px.

--- pages/404.tsx
+++ pages/404.tsx
@@ -32,39 +32,48 @@ const OhhhNoes: NextPage = () => {
       />
       <LayoutWrap bg="white">
         <LayoutInnerContent>
-          <Box display="flex" flexDirection="column" alignItems="center" width="100%">
-            <Box
-              as="h1"
-              fontSize={['800', null, '228.39px']}
-              lineHeight={['800', null, '285.49px']}
-              fontWeight="semibold"
-              mt={['0', null, '750']}
-            >
-              404
-            </Box>
-            <Box
-              as="p"
-              fontSize={['200', null, '600']}
-              lineHeight={['200', null, '600']}
-              textAlign="center"
-              mt={['100', null, '0']}
-            >
-              Oops! Looks like we can’t find the page you’re looking for.
-            </Box>
-            <Box
-              as="p"
-              fontSize={['200', null, '600']}
-              lineHeight={['200', null, '600']}
-              textAlign="center"
-              my={['400', null, '500']}
-            >
-              Return to{' '}
-              <Link href="/docs" passHref>
-                <StyledLink as="a" href="/docs">
-                  Home
-                </StyledLink>
-              </Link>
-              .
+          <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            width="100%"
+            maxWidth="86vh"
+            m="0 auto"
+          >
+            <Box>
+              <Box
+                as="h1"
+                fontSize={['800', null, 'clamp(3.5rem, 18vh, 14.27rem)']}
+                lineHeight={['800', null, 'clamp(3.5rem, 20vh, 17.84rem)']}
+                fontWeight="semibold"
+                textAlign="center"
+              >
+                404
+              </Box>
+              <Box
+                as="p"
+                fontSize={['200', null, 'clamp(1rem, 4vh, 2rem)']}
+                lineHeight={['200', null, 'clamp(1.25rem, 4.2vh, 2.5rem)']}
+                textAlign="center"
+                mt={['100', null, '0']}
+              >
+                Oops! Looks like we can’t find the page you’re looking for.
+              </Box>
+              <Box
+                as="p"
+                fontSize={['200', null, 'clamp(1rem, 4vh, 2rem)']}
+                lineHeight={['200', null, 'clamp(1.25rem, 4.2vh, 2.5rem)']}
+                textAlign="center"
+                my={['400', null, '500']}
+              >
+                Return to{' '}
+                <Link href="/docs" passHref>
+                  <StyledLink as="a" href="/docs">
+                    Home
+                  </StyledLink>
+                </Link>
+                .
+              </Box>
             </Box>
             <Box
               position="relative"
@@ -72,7 +81,8 @@ const OhhhNoes: NextPage = () => {
               justifyContent="center"
               width="100%"
               mt="500"
-              mb={['700', null, '434px']}
+              // mb={['700', null, '434px']}
+              mb="700"
             >
               <Image
                 className="next-image"
