b6f02bf2b72963075d99cc3a34915389545abb61
Jon Ambas <jon@jonambas.com>
2021-11-18 08:21:14 -0500

    WE-345 Make breadcrumbs and search categories friendly

--- pages/docs/index.tsx
+++ pages/docs/index.tsx
@@ -1,17 +1,20 @@
 import { GetStaticProps } from 'next';
+import { getCategorData } from 'lib/api';
 import SEO from 'components/site/seo';
 import DocsLayout from 'components/site/docsLayout';
 import { getSupportNavigation } from 'lib/api';
 import type { NavigationItemProps } from 'components/site/navigation';
+import { CategoriesProvider, Category } from 'context/categories';
 
 type IndexPageProps = {
   navigationData?: NavigationItemProps[];
+  categoryData: Category[];
 };
 
 const IndexPage = (props: IndexPageProps): JSX.Element => {
-  const { navigationData } = props;
+  const { categoryData, navigationData } = props;
   return (
-    <>
+    <CategoriesProvider data={categoryData}>
       <SEO
         title="Support Documentation - SparkPost"
         description="SparkPost Support Documentation"
@@ -19,13 +22,14 @@ const IndexPage = (props: IndexPageProps): JSX.Element => {
       <DocsLayout navigationData={navigationData}>
         <div>There's nothing here yet</div>
       </DocsLayout>
-    </>
+    </CategoriesProvider>
   );
 };
 
 export const getStaticProps: GetStaticProps = async () => {
   const navigationData = getSupportNavigation() || [];
-  return { props: { navigationData } };
+  const categoryData = getCategorData('docs');
+  return { props: { navigationData, categoryData } };
 };
 
 export default IndexPage;
--- pages/momentum/index.tsx
+++ pages/momentum/index.tsx
@@ -1,10 +1,18 @@
+import { GetStaticProps } from 'next';
+import { getCategorData } from 'lib/api';
 import MomentumHomePageContent from 'components/site/momentumHomePageContent';
 import SEO from 'components/site/seo';
 import MomentumLayout from 'components/site/momentumLayout';
+import { CategoriesProvider, Category } from 'context/categories';
 
-const IndexPage = (): JSX.Element => {
+type IndexPageProps = {
+  categoryData: Category[];
+};
+
+const IndexPage = (props: IndexPageProps): JSX.Element => {
+  const { categoryData } = props;
   return (
-    <>
+    <CategoriesProvider data={categoryData}>
       <SEO
         title="Momentum Documentation - SparkPost"
         description="SparkPost Momentum Documentation"
@@ -12,8 +20,13 @@ const IndexPage = (): JSX.Element => {
       <MomentumLayout>
         <MomentumHomePageContent />
       </MomentumLayout>
-    </>
+    </CategoriesProvider>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const categoryData = getCategorData('momentum');
+  return { props: { categoryData } };
 };
 
 export default IndexPage;
