d34d04709297f26b56faa310e5609dafd8113cb5
Max Harris <maxreuven@gmail.com>
2022-04-26 16:15:34 -0700

    removing https

--- components/site/gtm.tsx
+++ components/site/gtm.tsx
@@ -12,7 +12,7 @@ const GTM = (): JSX.Element => {
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','dataLayer', '${process.env.NEXT_PUBLIC_GTM_ID}');
             `,
           }}
