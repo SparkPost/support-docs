98080f26f6f9382de36ea3e3a93189a23c1b6f6a
shrirupa <shrirupa.chowdhury@sparkpost.com>
2022-06-15 16:33:26 -0400

    Add RudderStack and Remove Segment

--- utils/rudderstack.ts
+++ utils/rudderstack.ts
@@ -2,24 +2,24 @@ import { getWindow } from 'utils/ssr';
 
 declare global {
   interface Window {
-    analytics: {
+    rudderanalytics: {
       track: (event: string, properties: { [key: string]: string | boolean }) => void;
       page: (category?: string, name?: string) => void;
     };
   }
 }
 
-// Segment track
+// RudderStack track
 export const track = (event: string, properties: { [key: string]: string | boolean }) => {
   const environment = getWindow();
-  if (environment && environment.analytics) {
-    environment.analytics.track(event, properties);
+  if (environment && environment.rudderanalytics) {
+    environment.rudderanalytics.track(event, properties);
   }
 };
 
 export const page = (category?: string, name?: string) => {
   const environment = getWindow();
-  if (environment && environment.analytics) {
-    environment.analytics.page(category, name);
+  if (environment && environment.rudderanalytics) {
+    environment.rudderanalytics.page(category, name);
   }
 };
