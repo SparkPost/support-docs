63506dfa760993215c014f678bf5dc53cd6b11e0
shrirupa <shrirupa.chowdhury@sparkpost.com>
2022-06-22 11:45:49 -0400

    update cnames to sparkpost

--- components/site/rudderstack.tsx
+++ components/site/rudderstack.tsx
@@ -20,8 +20,8 @@ const RudderStack = (): JSX.Element => {
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-          !function(){var e=window.rudderanalytics=window.rudderanalytics||[];e.methods=["load","page","track","identify","alias","group","ready","reset","getAnonymousId","setAnonymousId"],e.factory=function(t){return function(){var r=Array.prototype.slice.call(arguments);return r.unshift(t),e.push(r),e}};for(var t=0;t<e.methods.length;t++){var r=e.methods[t];e[r]=e.factory(r)}e.loadJS=function(e,t){var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.rudderlabs.com/v1.1/rudder-analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a)},e.loadJS(),
-          e.load("2ACvzWWjRHtsQTDV3l0IcdAe21e","https://messagebird-dataplane.rudderstack.com"),
+          !function(){var e=window.rudderanalytics=window.rudderanalytics||[];e.methods=["load","page","track","identify","alias","group","ready","reset","getAnonymousId","setAnonymousId"],e.factory=function(t){return function(){var r=Array.prototype.slice.call(arguments);return r.unshift(t),e.push(r),e}};for(var t=0;t<e.methods.length;t++){var r=e.methods[t];e[r]=e.factory(r)}e.loadJS=function(e,t){var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://rs-cdn.analytics.sparkpost.com/v1.1/rudder-analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a)},e.loadJS(),
+          e.load("2ACvzWWjRHtsQTDV3l0IcdAe21e","https://rs-dataplane.analytics.sparkpost.com"),
           e.ready(() => {
               console.log("All set!")
           })
